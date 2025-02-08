3e2566bc103c6b457b2db08a931658d13b613634
archipelagoing <archisa28@gmail.com>
2025-02-08 16:51:17 -0500

    Removed Firebase service account key

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,12 @@
-#4:37p archisa 
+#4:42p archisa 
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
 
 # Load Firebase credentials
-cred = credentials.Certificate("serviceAccountKey.json")
+load_dotenv()
+cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 firebase_admin.initialize_app(cred)
 
 # Firestore database instance
